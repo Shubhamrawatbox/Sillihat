c74aaaba4501edf527484bda65ce500cdcc9136e
shbubham <shubhamrawatbox@gmail.com>
2021-09-28 20:09:38 +0530

    components change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 
-import Banner from "./Components/organism/Banner/Banner";
-import Clients from "./Components/organism/Clients/Clients";
-import FirstSection from "./Components/organism/FirstSection/FirstSection";
+import Banner from "./components/organism/Banner/Banner";
+import Clients from "./components/organism/Clients/Clients";
+import FirstSection from "./components/organism/FirstSection/FirstSection";
 
-import Navbar from "./Components/organism/Navbar/Navbar";
-import Payment from "./Components/organism/Payment/Payment";
-import Project from "./Components/organism/Project/Project";
-import Service from "./Components/organism/Service/Service";
-import Team from "./Components/organism/Team/Team";
-import Technology from "./Components/organism/Technology/Technology";
+import Navbar from "./components/organism/Navbar/Navbar";
+import Payment from "./components/organism/Payment/Payment";
+import Project from "./components/organism/Project/Project";
+import Service from "./components/organism/Service/Service";
+import Team from "./components/organism/Team/Team";
+import Technology from "./components/organism/Technology/Technology";
 import "./Sass/Global.scss";
 
 const App = () => {
